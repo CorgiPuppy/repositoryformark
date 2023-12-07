053a9b73811412c7ddc2ceda0f75342508ee6c19
Mokhov Mark <mohmagen@gmail.com>
2023-12-07 16:55:29 +0300

    bo

---
+++ main.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import keras 
+
+train_x = [[0,0],[0,1],[1,0],[1,1]] 
+train_y = [[0],[1],[1],[0]]
+
---
+++ mnist.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import keras as keras
+import pandas as pd
+
+
+data = pd.read_csv("./train.csv")
+print(data.describe())
+
+mnist = keras.datasets.mnist 
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=(28,28)),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(10),
+    ])
+
+predictions = model(x_train[:1]).numpy()
+print(predictions)
+
+print(tf.nn.softmax(predictions).numpy())
+
+loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+model.compile(optimizer='adam',loss=loss_fn,metrics=['accuracy'])
+history = model.fit(x_train, y_train, epochs=5)
+print(history)
+
+model.evaluate(x_test,  y_test, verbose=2)
